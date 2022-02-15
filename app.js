8f68e2201050498eaa9a995b639d4e83be9039a3
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-15 11:54:04 +0600

    background colors and text colors changed using js

---
+++ app.js
@@ -0,0 +1,8 @@
+const topPlayers = document.getElementById('top-players');
+topPlayers.style.color = 'gray';
+
+document.getElementById('top-blog').style.color = 'chocolate';
+const box = document.getElementsByClassName('box');
+for (const i of box) {
+    i.style.backgroundColor = 'rgba(255, 99, 71, 0.5)';
+}
