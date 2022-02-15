18d0862f6f77b5e9abc96cfe76302bf5fcbd23fe
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-02-15 12:11:16 +0600

    button for adding list elements created and functioned

--- app.js
+++ app.js
@@ -6,3 +6,13 @@ const box = document.getElementsByClassName('box');
 for (const i of box) {
     i.style.backgroundColor = 'rgba(255, 99, 71, 0.5)';
 }
+
+let count = 3;
+document.getElementById('add-list').addEventListener('click', () => {
+    count++;
+    const ul = document.getElementById('new-list');
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode('item -' +
+        count));
+    ul.appendChild(li);
+})
